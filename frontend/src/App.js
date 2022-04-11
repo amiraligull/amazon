74a7d94093aaf59c605ea8bbb160858b8041727b
amiraligull <amirgull42@gmail.com>
2022-04-11 22:18:43 +0500

    frontend setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+/** @format */
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Header</p>
+      </header>
+      <main>
+        <a href="https://reactjs.org">Products</a>
+      </main>
+    </div>
+  );
+}
+
+export default App;
